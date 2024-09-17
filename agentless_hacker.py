4168baa917ab0ae80e18b8bd2a476fd336ad1c3e
kannangog <realgod7t@gmail.com>
2024-09-17 10:48:08 +0530

    code modified

--- agentless_hacker.py
+++ agentless_hacker.py
@@ -5,31 +5,9 @@ import pyfiglet
 # Text to display
 text = "Agentless Hacker"
 
-# Get a list of all available fonts
-fonts = pyfiglet.FigletFont.getFonts()
-
-# All ASCII art and text from the "AL Hacker" theme
-al_hacker_ascii_art = [
-    r'''
-         .                                         .
-      .
-      dBBBBBBb  dBBBP dBBBBBBP dBBBBBb  .                       o
-       '   dB'                     BBP
-    dB'dB'dB' dBBP     dBP     dBP BB
-   dB'dB'dB' dBP      dBP     dBP  BB
-  dB'dB'dB' dBBBBP   dBP     dBBBBBBB
-
-                                   dBBBBBP  dBBBBBb  dBP    dBBBBP dBP dBBBBBBP
-          .                  .                  dB' dBP    dB'.BP
-                             |       dBP    dBBBB' dBP    dB'.BP dBP    dBP
-                           --o--    dBP    dBP    dBP    dB'.BP dBP    dBP
-                             |     dBBBBP dBP    dBBBBP dBBBBP dBP    dBP
-
-                                                                    .
-                .
-        o                  To boldly go where no
-                            shell has gone before
-    ''',
+# All ASCII art and text from the Metasploit theme, Rabbit, Skull, and "I love you" designs
+ascii_art_list = [
+    # AL Hacker 2
     r'''
                .;lxO0KXXXK0Oxl:.
            ,o0WMMMMMMMMMMMMMMMMMMKd,
@@ -56,40 +34,67 @@ xMMMMMMMMMd                        ,0MMMMMMMMMMK;
                .,cdkO0K;        :+:    :+:                                
                                 :::::::+:
                       AL Hacker
-
-
-Call trans opt: received. 2-19-98 13:24:18 REC:Loc
-
-     Trace program: running
-
-           wake up, Neo...
-        the matrix has you
-      follow the white rabbit.
-
-          knock, knock, Neo.
     ''',
+    # Metasploit ASCII Art
     r'''
-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-MMMMMMMMMMM                MMMMMMMMMM
-MMMN$                           vMMMM
-MMMNl  MMMMM             MMMMM  JMMMM
-MMMNl  MMMMMMMN       NMMMMMMM  JMMMM
-MMMNl  MMMMMMMMMNmmmNMMMMMMMMM  JMMMM
-MMMNI  MMMMMMMMMMMMMMMMMMMMMMM  jMMMM
-MMMNI  MMMMMMMMMMMMMMMMMMMMMMM  jMMMM
-MMMNI  MMMMM   MMMMMMM   MMMMM  jMMMM
-MMMNI  MMMMM   MMMMMMM   MMMMM  jMMMM
-MMMNI  MMMNM   MMMMMMM   MMMMM  jMMMM
-MMMNI  WMMMM   MMMMMMM   MMMM#  JMMMM
-MMMMR  ?MMNM             MMMMM .dMMMM
-MMMMNm `?MMM             MMMM` dMMMMM
-MMMMMMN  ?MM             MM?  NMMMMMN
-MMMMMMMMNe                 JMMMMMNMMM
-MMMMMMMMMMNm,            eMMMMMNMMNMM
-MMMMNNMNMMMMMNx        MMMMMMNMMNMMNM
-MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM
-        https://AL-Hacker.com
+         d88888b db    db d88888b d8b   db d888888b d888888b 
+         88'     88    88 88'     888o  88   `88'   `~~88~~' 
+         88ooooo 88    88 88ooooo 88V8o 88    88       88    
+         88~~~~~ 88    88 88~~~~~ 88 V8o88    88       88    
+         88.     88b  d88 88.     88  V888   .88.      88    
+         Y88888P ~Y8888P' Y88888P VP   V8P Y888888P    YP    
+                Metasploit by Rapid7     
     ''',
+    # Skull ASCII Art
+    r'''
+                      ########                  #
+                      #################            #
+                   ######################         #
+                  #########################      #
+                ############################
+               ##############################
+               ###############################
+              ###############################
+              ##############################
+                              #    ########   #
+                 ##        ###        ####   ##
+                                      ###   ###
+                                    ####   ###
+               ####          ##########   ####
+               #######################   ####
+                 ####################   ####
+                  ##################  ####
+                    ############      ##
+                       ########        ###
+                      #########        #####
+                    ############      ######
+                   ########      #########
+                     #####       ########
+                       ###       #########
+                      ######    ############
+                     #######################
+                     #   #   ###  #   #   ##
+                     ########################
+                      ##     ##   ##     ##
+                            https://Agentless-Hacker.com
+    ''',
+    # Rabbit ASCII Art
+    r'''
+                        (`.         ,-,
+                        ` `.    ,;' /
+                         `.  ,'/ .'
+                          `. X /.'
+                .-;--''--.._` ` (
+              .'            /   `
+             ,           ` '   Q '            ======Agentless Hacker======
+             ,         ,   `._    \
+          ,.|         '     `-.;_'
+           ' `    ,   )   .'
+              `._ ,  '   /_
+                 ; ,''-,;' ``-
+                  ``-..__``--`
+    ''',
+    # I love you ASCII Art
     r'''
 IIIIII    dTb.dTb        _.---._
   II     4'  v  'B   .'"".'/|\`.""'.
@@ -98,47 +103,30 @@ IIIIII    dTb.dTb        _.---._
   II      'T; ;P'    `. /   |   \ .'
 IIIIII     'YvP'       `-.__|__.-'
 
-I love shells --egypt
-    ''',
-    r'''
-                                .,,.                  .                                                                                                                                                 
-                             .\$$$$$L..,,==aaccaacc%#s$b.       d8,    d8P                                                                                                                              
-                  d8P        #$$$$$$$$$$$$$$$$$$$$$$$$$$$b.    `BP  d888888p                                                                                                                            
-               d888888P      '7$$$$\""""''^^`` .7$$$|D*"'```         ?88'                                                                                                                               
-   d8bd8b.d8p d8888b ?88' d888b8b            _.os#$|8*"`   d8P       ?8b  88P                                                                                                                           
-   88P`?P'?P d8b_,dP 88P d8P' ?88       .oaS###S*"`       d8P d8888b $whi?88b 88b                                                                                                                       
- d88  d8 ?8 88b     88b 88b  ,88b .osS$$$$*" ?88,.d88b, d88 d8P' ?88 88P `?8b                                                                                                                            
-d88' d88b 8b`?8888P'`?8b`?88P'.aS$$$$Q*"`    `?88'  ?88 ?88 88b  d88 d88                                                                                                                               
-                          .a#$$$$$$"`          88b  d8P  88b`?8888P'
-                       ,s$$$$$$$"`             888888P'   88n      _.,,,ass;:
-                    .a$$$$$$$P`               d88P'    .,.ass%#S$$$$$$$$$$$$$$'                                                                                                                        
-                 .a$###$$$P`           _.,,-aqsc#SS$$$$$$$$$$$$$$$$$$$$$$$$$$'                                                                                                                          
-              ,a$$###$$P`  _.,-ass#S$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$####SSSS'
-           .a$$$$$$$$$$SSS$$$$$$$$$$$$$$$$$$$$$$$$$$$$SS##==--""''^^/$$$$$$'
-______________________________________________________________________________                                                                                                                        
-                                                             AL-Hacker.com
+I love shells --egypt @ Agentless-Hacker
     '''
 ]
 
 # Function to create and display random text art
 def generate_text_art(text):
+    fonts = pyfiglet.FigletFont.getFonts()
     random_font = random.choice(fonts)
     fig = pyfiglet.Figlet(font=random_font)
     art_text = fig.renderText(text)
     return art_text
 
-# Function to display a random AL Hacker ASCII image
-def display_random_al_hacker_image():
-    random_image = random.choice(al_hacker_ascii_art)
+# Function to display a random ASCII image
+def display_random_ascii_image():
+    random_image = random.choice(ascii_art_list)
     print(random_image)
 
 # Generate and display the text art
 print(generate_text_art(text))
 
-# Display a random AL Hacker image below the text
-display_random_al_hacker_image()
+# Display a random ASCII image below the text
+display_random_ascii_image()
 
-option=print(input("Enter y for ipconfig::"))
-if(option=="y"or"Y"):
+# Run system command if user chooses
+option = input("Enter y for ipconfig: ")
+if option.lower() == 'y':
     subprocess.call("ipconfig")
-
